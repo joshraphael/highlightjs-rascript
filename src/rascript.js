af0e94ebc7fad3d61eed48889aa6676b80732c7a
Joshua Raphael <josh.n.raphael@gmail.com>
2025-08-31 13:57:49 -0700

    add majority of syntax for language

--- src/rascript.js
+++ src/rascript.js
@@ -5,12 +5,59 @@
  */
 function RAScript(hljs) {
     return {
-        case_insensitive: true,
-        keywords: [
-            'else',
-            'for',
-            'if',
-            'while'
+        case_insensitive: false,
+        contains: [
+            hljs.C_LINE_COMMENT_MODE,
+            hljs.QUOTE_STRING_MODE,
+            hljs.C_NUMBER_MODE,
+            hljs.COMMENT(
+                '/\\*',
+                '\\*/',
+            ),
+            {
+                className: 'variable.language',
+                begin: /\b(this)\b/
+            },
+            {
+                className: 'keyword',
+                begin: /\b(function|class|else|for|if|in|return)\b/
+            },
+            {
+                className: 'literal',
+                begin: /\b(true|false)\b/
+            },
+            {
+                begin: [
+                    /function[\t ]+/,
+                    /[a-zA-Z_][\w]*/,
+                    /\(/
+                ],
+                beginScope: {
+                    2: "title.function"
+                },
+            },
+            {
+                begin: [
+                    /class[\t ]+/,
+                    /[a-zA-Z_][\w]*/
+                ],
+                beginScope: {
+                    2: "title.class"
+                },
+            },
+            {
+                begin: [
+                    /[a-zA-Z_][\w]*/,
+                    /\(/
+                ],
+                beginScope: {
+                    1: "title.function.invoke"
+                },
+            },
+            {
+                className: 'variable',
+                begin: /[a-zA-Z_][\w]*/,
+            }
         ]
     }
 }
